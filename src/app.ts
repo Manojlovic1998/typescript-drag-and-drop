f34da865b26da45833e5bf11fe868e353e8be968
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-30 18:51:52 +0200

    Crate ProjectList class

--- src/app.ts
+++ src/app.ts
@@ -63,6 +63,40 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// ProjectList Class
+class ProjectList {
+  element: HTMLElement;
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-list")
+    );
+    this.hostElement = <HTMLDivElement>document.getElementById("app");
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = this.type + " Projects";
+  }
+
+  private attach(): void {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -160,3 +194,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
