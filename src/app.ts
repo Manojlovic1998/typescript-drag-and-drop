a4bd0813f549eae4e61f103ecce321d7358df84e
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-30 14:16:02 +0200

    Create autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+// Auto bind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -32,6 +47,7 @@ class ProjectInput {
     this.attach();
   }
 
+  @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
     console.log(this.titleInputElement.value);
@@ -39,7 +55,7 @@ class ProjectInput {
 
   private configure() {
     console.log(this.element);
-    this.element.addEventListener("submit", this.submitHandler.bind(this));
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach(): void {
