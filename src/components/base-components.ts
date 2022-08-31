ede77e888192c5122aaed98ba8aa285ecfce4d84
Nemanja Manojlovic <manojlovic911@hotmail.com>
2022-08-31 21:42:56 +0200

    Use namespace to split the code logic into different .ts modules/files

---
+++ src/components/base-components.ts
@@ -0,0 +1,48 @@
+namespace App {
+  // Component Base Class
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      // Template
+      this.templateElement = document.getElementById(
+        templateId
+      ) as HTMLTemplateElement;
+      // Node that will host template elements
+      this.hostElement = document.getElementById(hostElementId) as T;
+      // Creating a DocumentFragment using the templateElement content
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      // Getting the first nested child of the template content
+      this.element = importedNode.firstElementChild as U;
+      // If available set the id value of the new element
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAfterBegin: boolean): void {
+      this.hostElement.insertAdjacentElement(
+        insertAfterBegin ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
---
+++ src/state/project-state.ts
@@ -0,0 +1,67 @@
+namespace App {
+  // Project State Management Class
+  type Listener<T> = (items: T[]) => void;
+
+  abstract class State<T> {
+    // List of function references
+    protected listeners: Listener<T>[] = [];
+
+    addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends State<Project> {
+    // List of projects
+    private projects: Project[] = [];
+    // Singleton instance
+    private static instance: ProjectState;
+    // Private constructor declaration
+    private constructor() {
+      super();
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+      // Create new object with the use of func. args
+      const newProject = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        numOfPeople,
+        ProjectStatus.Active
+      );
+      // Push to projects list
+      this.projects.push(newProject);
+      this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const project = this.projects.find((prj) => {
+        return prj.id === projectId;
+      });
+
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    // Singleton getter/setter
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      }
+      this.instance = new ProjectState();
+      return this.instance;
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        // Returns a copy so we don't edit from the outside
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+  // Singleton State
+  export const projectState = ProjectState.getInstance();
+}
